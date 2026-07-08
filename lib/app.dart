8167669f52f6e7a0eef869fa9a11b663d0644ca5
Navaneeth P P <appunavaneeth0@gmail.com>
2026-07-08 12:34:16 +0530

    feat: initialize application structure with root app widget and splash screen component

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:docbridgeconnect/core/theme/app_theme.dart';
+import 'package:docbridgeconnect/features/splash/splashScreen.dart';
+
+import 'package:flutter/material.dart';
+
+class DocBridgeApp extends StatelessWidget {
+  const DocBridgeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      title: 'DocBridge',
+
+      theme: AppTheme.dark,
+
+      home: const Splashscreen(),
+    );
+  }
+}
