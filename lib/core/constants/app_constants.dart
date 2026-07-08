841c02d3ffa813cd6048455a13ce03283ec88f0f
Navaneeth P P <appunavaneeth0@gmail.com>
2026-07-08 15:09:43 +0530

    feat: implement splash screen UI and add app tagline and version constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,4 +2,7 @@ class Appconstants {
   Appconstants._();
 
   static const String appName = 'DocBridge Connect';
+  static const String appTagLine =
+      'Professional Wireless Document Scanner';
+  static const String appVersion = '1.0.0';
 }
