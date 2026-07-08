7129c6435e53c8abbf6eae278052714e0aa45a65
Navaneeth P P <appunavaneeth0@gmail.com>
2026-07-08 11:39:03 +0530

    feat: define color palette constants in AppColors class

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  //background
+  static const background = Color(0xFF141218);
+  static const surface = Color(0xFF1D1B20);
+  static const card = Color(0xFF211F24);
+
+  // Primary
+  static const primary = Color(0xFF00E5A8);
+  static const secondary = Color(0xFF3B82F6);
+
+  // Text
+  static const textPrimary = Color(0xFFE6E0E9);
+  static const textSecondary = Color(0xFFCBC4D2);
+
+  // Status
+  static const success = Color(0xFF10B981);
+  static const warning = Color(0xFFF59E0B);
+  static const error = Color(0xFFEF4444);
+
+  // Border
+  static const border = Color(0x14FFFFFF);
+
+  // Glass
+  static const glass = Color(0xB3211F24);
+}
