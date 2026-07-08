089e61dd8d76335d90bacf2853e0113b0ed6415b
Navaneeth P P <appunavaneeth0@gmail.com>
2026-07-08 11:59:45 +0530

    feat: add google_fonts, jni support for desktop, and define AppSpacing constants

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,12 @@
+class AppSpacing {
+  AppSpacing._();
+
+  static const unit = 4.0;
+  static const xs = 8.0;
+  static const sm = 16.0;
+  static const md = 24.0;
+  static const lg = 32.0;
+  static const xl = 40.0;
+
+  static const screenPadding = 24.0;
+}
