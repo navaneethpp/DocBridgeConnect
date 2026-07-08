fb99570a799b9ea3e1a93c4ff52a6d05788517ba
Navaneeth P P <appunavaneeth0@gmail.com>
2026-07-08 12:35:24 +0530

    refactor: move app widget to separate file and initialize binding in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
+import 'package:docbridgeconnect/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const Text('main.dart'),
-    );
-  }
+  runApp(const DocBridgeApp());
 }
