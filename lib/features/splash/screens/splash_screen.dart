fe84f795694cf9486c588436edda2ad410e2630c
Navaneeth P P <appunavaneeth0@gmail.com>
2026-07-11 12:54:07 +0530

    feat: implement SplashController to manage initialization and navigation logic in SplashScreen

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,13 +1,32 @@
 import 'package:docbridgeconnect/core/theme/app_colors.dart';
 import 'package:docbridgeconnect/core/theme/app_spacing.dart';
+import 'package:docbridgeconnect/features/splash/controller/splash_controller.dart';
 import 'package:docbridgeconnect/features/splash/widgets/animated_logo.dart';
 import 'package:docbridgeconnect/features/splash/widgets/loading_pill.dart';
 import 'package:docbridgeconnect/features/splash/widgets/splash_background.dart';
 
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late final SplashController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = SplashController(context);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.initialize();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
